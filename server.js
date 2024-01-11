ed998df8c2a808faa537158cddf2a80b285e19ca
Ash-2k3 <ash.me.cse@gmail.com>
2024-01-11 23:14:17 +0530

    Basic server setup

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.port || 4000;
+
+app.listen(port, () => {
+           console.log(`App is running on port ${port}`);
+})
