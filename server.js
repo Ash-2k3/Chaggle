b7805043bedaceac849c9043a0696c400eca6203
Ash-2k3 <ash.me.cse@gmail.com>
2024-01-12 18:33:58 +0530

    Import socketio and integrate server with connections

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const http = require('http'); // Using an http server is a common practise to manage real time communication with WebSocket.
+const socketIo = require('socket.io');
 
 const app = express();
+const server = http.createServer(app); // Passed the express instance to the http server instance. Express application is then configured to use this server instance. This ensures that server handles both HTTP requests(handled by Express) and WebSocket Communication.
+const io = socketIo(server); // Integrating socket.io with the server
 
 const port = process.env.port || 4000;
 
-app.listen(port, () => {
+server.listen(port, () => {
            console.log(`App is running on port ${port}`);
-})
+});
+
+app.get('/', (req, res) => {
+           res.send('<h1>Hello World</h1>');
+});
