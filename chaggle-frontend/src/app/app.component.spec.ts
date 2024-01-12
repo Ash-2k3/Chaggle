c248aa473e4f7114dbc5131dd15fc0fb19dda1fa
Ash-2k3 <ash.me.cse@gmail.com>
2024-01-13 00:04:53 +0530

    Update frontend file structure

--- chaggle-frontend/src/app/app.component.spec.ts
+++ chaggle-frontend/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'video-chat-app'`, () => {
+  it(`should have as title 'chaggle-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('video-chat-app');
+    expect(app.title).toEqual('chaggle-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('video-chat-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('chaggle-frontend app is running!');
   });
 });
